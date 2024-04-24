7fbd41bd62dd947f84cc68e6bdcd13c06246bddc
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-24 14:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+  let splitStr = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+    let count = 0;
+    for (let j = 0; j < splitStr.length; j++) {
+      if (char == splitStr[j]) {
+        count++;
+      }
+    }
+    if (count === 1) {
+      return char;
+    }
+  }
+  return null;
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
